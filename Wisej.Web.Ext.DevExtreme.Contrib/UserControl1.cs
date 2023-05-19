4ce1060d42e6992f3421038b5178b0683d3f2338
Kürşat Turgut <kursat.turgut@lefyapi.com.tr>
2023-05-19 12:10:00 +0300

    Add project files.

---
+++ Wisej.Web.Ext.DevExtreme.Contrib/UserControl1.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Wisej.Web;
+
+namespace Wisej.Web.Ext.DevExtreme.Contrib
+{
+    public partial class UserControl1 : Wisej.Web.UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
